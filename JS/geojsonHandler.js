a2b84071af72b05ba345dc652cf168f641c2a276
Ŝeverin Fritschi <severin.fritschi@hsr.ch>
2017-04-07 16:43:56 +0200

    Added getGeojson functionality

--- JS/geojsonHandler.js
+++ JS/geojsonHandler.js
@@ -1,14 +1,29 @@
 var loadGeojson = function(mapObject, path) {	
 	var geoJson = new L.geoJson();
 		geoJson.addTo(mapObject);
-
+	
 	$.ajax({
-	dataType: "json",
-	url: path,
-	success: function(data) {
-		$(data.features).each(function(key, data) {
-			geoJson.addData(data);
-		});
-	}
+		dataType: "json",
+		url: path,
+		success: function(data) {
+			$(data.features).each(function(key, data) {
+				geoJson.addData(data);
+			});
+		}
 	}).error(function() {});
+}
+
+var getGeojson = function(path) {
+	var geoJson = new L.geoJson();
+	
+	$.ajax({
+		dataType: "json",
+		url: path,
+		success: function(data) {
+			$(data.features).each(function(key, data) {
+				geoJson.addData(data);
+			});
+		}
+	}).error(function() {});
+	return geoJson;
 }
