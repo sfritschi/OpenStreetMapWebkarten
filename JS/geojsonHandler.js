5565d063dfe3824610d809d5f02fe6282a183946
Ŝeverin Fritschi <severin.fritschi@hsr.ch>
2017-04-11 17:25:25 +0200

    Removed unused plugin files, added castle clusters

--- JS/geojsonHandler.js
+++ JS/geojsonHandler.js
@@ -12,3 +12,22 @@ var getGeojson = function(path) {
 	}).error(function() {});
 	return geoJson;
 }
+
+var castleMarkers = L.markerClusterGroup();
+
+var loadGeojson = function(filepath, layergoup, icon){
+    $.getJSON(filepath, function(data) {
+		castleMarkers = L.markerClusterGroup();
+        L.geoJson(data, {
+            //go through every element of the json and add it to the layer
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup(feature.properties.name ? feature.properties.name : "Kein Name");
+            },
+            pointToLayer: function (feature, latlng) {
+				castleMarkers.addLayer(L.marker(latlng, {icon: icon }));
+            }
+        });
+        castleMarkers.addTo(layergoup);
+    });
+    return layergoup;
+};
