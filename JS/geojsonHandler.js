c4fa4acdbb5711046cf2a83ef7c3dcc3ec6b8f92
Ŝeverin Fritschi <severin.fritschi@hsr.ch>
2017-04-11 08:57:22 +0200

    Added html image map

--- JS/geojsonHandler.js
+++ JS/geojsonHandler.js
@@ -1,18 +1,3 @@
-var loadGeojson = function(mapObject, path) {	
-	var geoJson = new L.geoJson();
-		geoJson.addTo(mapObject);
-	
-	$.ajax({
-		dataType: "json",
-		url: path,
-		success: function(data) {
-			$(data.features).each(function(key, data) {
-				geoJson.addData(data);
-			});
-		}
-	}).error(function() {});
-}
-
 var getGeojson = function(path) {
 	var geoJson = new L.geoJson();
 	
